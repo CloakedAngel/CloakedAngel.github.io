ebfa94aade2606e91c6653655a56a8627a6d6e2a
CloakedAngel <aydenfujikawa@gmail.com>
2022-06-08 16:11:16 +0000

    saving walker

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -10,14 +10,24 @@ function runProgram(){
   // Constant Variables
   var FRAME_RATE = 60;
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
-  
+  var KEY = {
+    ENTER: 13,
+    LEFT: 37,
+    UP: 38,
+    RIGHT: 39,
+    DOWN: 40,
+}
+  var positionX = 0; //  x-coordinate 
+  var speedX = 0; //  speed along the x-axis
+  var positionY = 0; //  y-coordinate 
+  var speedY = 0; //  speed along the y-axis
   // Game Item Objects
 
 
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
-
+  $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keyup', handleKeyUp);
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -27,22 +37,71 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    
-
+    redrawGameItem(), repositionGameItem(), borderPatrolX(), borderPatrolY()
   }
   
   /* 
   Called in response to events.
   */
-  function handleEvent(event) {
-
+  function handleKeyDown(event) {
+    if (event.which === KEY.ENTER){
+      console.log('enter');
+    } else if (event.which === KEY.LEFT){
+      speedX = -5;
+    } else if (event.which === KEY.UP){
+      speedY = -5;
+    } else if (event.which === KEY.RIGHT){
+      speedX = 5;
+    } else if (event.which === KEY.DOWN){
+      speedY = 5;
+    }
   }
+
+    function handleKeyUp(event) {
+      if (event.which === KEY.ENTER){
+        console.log('enter');
+      } else if (event.which === KEY.LEFT){
+        speedX = 0;
+      } else if (event.which === KEY.UP){
+        speedY = 0;
+      } else if (event.which === KEY.RIGHT){
+        speedX = 0;
+      } else if (event.which === KEY.DOWN){
+        speedY = 0;
+      }
+    }
+
+    function borderPatrolX() {
+      if (positionX < 3) {
+      positionX = -1.5;
+      } else if (positionX > 390) {
+        positionX = 391;
+      } 
+    }
+
+    function borderPatrolY() {
+      if (positionY < 3) {
+      positionY = -1.5;
+      } else if (positionY > 390) {
+        positionY = 391;
+      } 
+    }
 
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
 
+
+  function repositionGameItem(){
+    positionX += speedX; // update the position along the x-axis
+    positionY += speedY; // update the position along the y-axis
+  }
   
+  function redrawGameItem() {
+    $("#walker").css("left", positionX);    // draw in the new location, positionX pixels away from the "left"
+    $("#walker").css("top", positionY);    // draw in the new location, positionY pixels away from the "left"
+  }
+
   function endGame() {
     // stop the interval timer
     clearInterval(interval);
